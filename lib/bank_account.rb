39b8f677d7e798e4362bd9eefa4b5830698b945c
Victor Ibasco <victoribasco18@gmail.com>
2020-02-13 21:07:08 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,23 +1,36 @@
 class BankAccount
-  attr_accessor :balance
-  attr_reader :name, :status
+  attr_accessor :balance, :status
+  attr_reader :name
   
-  def initialize(name)
+  def initialize(name, balance = 1000, status = "open")
     @name = name
-    @balance = 1000
-    @status = "open"
+    @balance = balance
+    @status = status
   end
 
   def deposit(amnt)
     @balance += amnt
   end
   
+  def withdraw(amnt)
+    @balance -= amnt
+  end
+  
   def display_balance
     return "Your balance is $#{@balance}."
   end
   
-  def valid?()
-    self.status == "open" && self.balance > 0 ? true : false
+  def transfer(receiver, transfer_amnt)
+    receiver.deposit(transfer_amnt)
+    withdraw(transfer_amnt)
+  end
+  
+  # def valid?
+  #   self.status == "open" && self.balance > 0 ? true : false
+  # end
+  
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
   end
   
   def close_account
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,36 @@
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
   
-  def initialize(sender, receiver, transfer_amnt)
+  def initialize(sender, receiver, status = "pending", transfer_amnt)
     @sender = sender
     @receiver = receiver
-    @status = 'pending'
-    @transfer_amnt = transfer_amnt
+    @status = status
+    @amount = transfer_amnt
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid? ? true : false
+  end
+  
+  def execute_transaction
+    if (valid? && sender.balance > amount) && self.status == "pending"
+      sender.transfer(@receiver, amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if (valid? && receiver.balance > amount) && self.status == "complete"
+      receiver.withdraw(amount)
+      sender.deposit(amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
   
 end
